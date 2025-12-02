7b1b8b8b6d16a6712390fcd0625f5beaaaf914d1
faizfazdlih <faizfazdlih8@gmail.com>
2025-12-02 21:58:46 +0700

    dark mode

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,50 +1,27 @@
 "use client"
 
 import * as React from "react"
-import { Orbit, Eclipse } from "lucide-react"
+import { Sun, Moon } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-} from "@/components/ui/dropdown-menu"
 
 export function ThemeToggleButton() {
-  const {theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  function toggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="sm">
-          <Eclipse className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Orbit className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Theme</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-
-        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="light">
-            Light
-          </DropdownMenuRadioItem>
-
-          <DropdownMenuRadioItem value="dark">
-            Dark
-          </DropdownMenuRadioItem>
-
-        < DropdownMenuRadioItem value="system">
-            System
-          </DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="sm"
+      onClick={toggleTheme}
+      className="relative"
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
